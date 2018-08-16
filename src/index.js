db695956841c6c92b0160faadd15a5f5659d4d6a
Jared Palmer <jared@palmer.net>
2018-08-16 09:33:07 -0400

    Use export *

--- src/index.js
+++ src/index.js
@@ -1,8 +1,3 @@
-import React from 'react';
-import { cache as CacheSrc } from './cache';
-import { Img as ImgSrc } from './Img';
-import { Script as ScriptSrc } from './Script';
-
-export const cache = CacheSrc;
-export const Img = ImgSrc;
-export const Script = ScriptSrc;
+export * from './Video';
+export * from './Img';
+export * from './Script';
