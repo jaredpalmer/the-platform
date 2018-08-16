ae60eb8acb11faac3f02049ff75204583ac34d7b
Jared Palmer <jared@palmer.net>
2018-08-16 19:12:22 -0400

    what did we do here?

--- src/Img.js
+++ src/Img.js
@@ -2,12 +2,20 @@ import React from 'react';
 import { createResource } from 'simple-cache-provider';
 import { cache } from './shared';
 
-function load(image) {
-  const { src } = image;
+function load(attributes) {
+  const { src, onLoad, onError, className, ...attrs } = attributes;
   return new Promise((resolve, reject) => {
     const image = new Image();
-    image.onload = resolve;
-    image.onerror = reject;
+    Object.keys(attrs).forEach(name => image.setAttribute(name, attrs[name]));
+    image.onload = e => {
+      onLoad(e);
+      resolve();
+    };
+    image.onerror = e => {
+      onError(e);
+      reject();
+    };
+    image.className = className;
     image.src = src;
   });
 }
@@ -16,7 +24,9 @@ const resource = createResource(load, ({ src }) => src);
 
 export const Img = ({ cache, ...props }) => {
   resource.read(cache, props);
-  return <img {...props} />;
+  // intercept
+  const { onError, ...rest } = props;
+  return <img {...rest} />;
 };
 
 Img.defaultProps = {
