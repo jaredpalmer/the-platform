91d4920694ba4b018d3f669c305a57e48ef9264e
Birkir Rafn Guðjónsson <birkir.gudjonsson@gmail.com>
2018-10-28 19:02:12 -0400

    Use only one resize event handler for all effects

--- src/useWindowSize.js
+++ src/useWindowSize.js
@@ -1,6 +1,9 @@
 import * as React from 'react';
 import throttle from 'lodash/throttle';
 
+const events = new Set();
+const onResize = () => events.forEach(fn => fn());
+
 export const useWindowSize = (options = {}) => {
   const { throttleMs = 100 } = options;
   const [size, setSize] = React.useState({
@@ -16,10 +19,18 @@ export const useWindowSize = (options = {}) => {
   }, throttleMs);
 
   React.useEffect(() => {
-    window.addEventListener('resize', handle);
-
+    if (events.size === 0) {
+      window.addEventListener('resize', onResize, true);
+    }
+    
+    events.add(handle);
+    
     return () => {
-      window.removeEventListener('resize', handle);
+      events.delete(handle);
+
+      if (events.size === 0) {
+        window.removeEventListener('resize', onResize, true);
+      }
     };
   }, []);
 
