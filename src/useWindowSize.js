292f9b20ead903e946c7c7c15188d8ad118a88e8
Steven <steven@ceriously.com>
2018-10-29 15:40:57 -0400

    Change import from lodash.throttle

--- src/useWindowSize.js
+++ src/useWindowSize.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import throttle from 'lodash/throttle';
+import throttle from 'lodash.throttle';
 
 export const useWindowSize = (options = {}) => {
   const { throttleMs = 100 } = options;
