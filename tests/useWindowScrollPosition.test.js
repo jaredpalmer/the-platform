af13763a48777fef4fb5ce6fb3b3a6213963ff1c
emilec <emilec@corp.doctor.com>
2018-11-05 23:26:01 -0800

    test useState portion of useWindowScrollPosition hook

---
+++ tests/useWindowScrollPosition.test.js
@@ -0,0 +1,26 @@
+import { useWindowScrollPosition } from '../src/useWindowScrollPosition';
+
+function App() {
+  const { x, y } = useWindowScrollPosition();
+
+  return (
+    <h1 data-testid="test-header">{`pageXOffset is ${x}, pageYOffset is ${y}`}</h1>
+  );
+}
+
+afterEach(cleanup);
+
+describe('useWindowScrollPosition()', () => {
+  test('returns correct values', () => {
+    // set starting scroll position
+    window.pageXOffset = 50;
+    window.pageYOffset = 100;
+
+    const { getByTestId } = render(<App />);
+
+    // assert that the useState portion of this hook returns the proper values
+    expect(getByTestId('test-header')).toHaveTextContent(
+      'pageXOffset is 50, pageYOffset is 100'
+    );
+  });
+});
