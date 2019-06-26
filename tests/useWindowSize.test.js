f721b0a2e44a86b4e5c0474c592ac46b3827ba9a
Alex Sandiiarov <sandiyarov@gmail.com>
2019-06-26 18:40:24 -0400

    test: remove useless import Simulate (#75)
    * test: remove useless import Simulate
    
    * test: remove useless import Simulate

--- tests/useWindowSize.test.js
+++ tests/useWindowSize.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, Simulate, cleanup } from 'react-testing-library';
+import { render, cleanup } from 'react-testing-library';
 import { useWindowSize } from '../src/useWindowSize';
 
 function App() {
